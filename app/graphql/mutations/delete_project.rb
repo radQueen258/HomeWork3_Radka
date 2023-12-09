afd4294387eb7487581b608e395d59c9f07737ad
Radka Nhapulo <radka.fnhapulo@gmail.com>
2023-12-09 20:50:52 +0300

    delete project mutation

--- app/graphql/mutations/delete_project.rb
+++ app/graphql/mutations/delete_project.rb
@@ -1,13 +1,14 @@
 module Mutations
   class DeleteProject < BaseMutation
-    argument :input, Types::Inputs::DeleteProjectInput, required: true
+    argument :id, Types::Inputs::DeleteProjectInput, required: true
 
     type Types::Payloads::ProjectPayload
 
-    def resolve(input:)
+    def resolve(id:)
       current_user = context[:current_user]
-      result = Projects::DeleteProject.call(project: ::Project.find(:id),
-        project_params:project_params, current_user: current_user)
+      project = Project.find_by(id: id)
+
+      result = Projects::DeleteProject.call(project: project, current_user: current_user)
 
         result.to_h.merge(errors: formatted_errors(result.project))
     end
